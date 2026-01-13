5b5d83845acfa3dbc0f3c094b59ec4fa5b7696c3
Ishank Jain <jainishank272@gmail.com>
2026-01-13 12:34:36 +0530

    first initialization in the stockflow proj

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+// import Image from "next/image";
+
+export default function Ishank() {
+    return (
+        <main className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <div className="text-center text-5xl">
+                Ishank Jain
+            </div>
+        </main>
+    );
+}
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,7 @@
+export default function SignIn(){
+    return(
+        <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-purple-50 to-purple-100 text-black text-3xl">
+            Sign In page
+        </div>
+    )
+}
